ed1e89192be8c86eefc96cae7a8cc0ae65051f0a
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-18 09:56:55 +0300

    Add controller

---
+++ PlainModelTest/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PlainModelTest.Model;
+
+namespace PlainModelTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            Company company = new Company
+            {
+                Name = "Microsoft",
+                Employees = 10000
+            };
+            string result = $"Company name = {company.Name}, enployees = {company.Employees}";
+            return result;
+        }
+    }
+}
