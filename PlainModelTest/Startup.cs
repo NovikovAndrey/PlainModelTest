e10c9d1a779972db4fbe2f722509d404a57e39aa
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-18 10:12:56 +0300

    Add EndPointRouting-False

--- PlainModelTest/Startup.cs
+++ PlainModelTest/Startup.cs
@@ -16,12 +16,15 @@ namespace PlainModelTest
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(option=> option.EnableEndpointRouting = false);
+            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
+            
             app.UseMvc(routes =>
             {
                 routes.MapRoute("defaulty", "{controller=Home}/{action=Index}/{id?}");
