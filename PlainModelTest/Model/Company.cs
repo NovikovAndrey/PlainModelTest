a59fde54a3a6481acd09ff84ce23223f7fd4ce24
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-18 09:50:01 +0300

    Add model

---
+++ PlainModelTest/Model/Company.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlainModelTest.Model
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public int Employees { get; set; }
+
+    }
+}
